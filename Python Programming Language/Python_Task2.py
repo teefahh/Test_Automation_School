c24d046ad0a53022e68f505f3f587c4005053d3b
Latifat Yisa <latifat.yisa@accelerexholdings.com>
2022-10-13 22:27:52 +0100

    Python Task_2

---
+++ Python Programming Language/Python_Task2.py
@@ -0,0 +1,15 @@
+#Python Programming Task 2
+
+#1. Declaring a variable using the variable name my_var
+my_var = 12.6
+print(type(my_var))
+
+#2. Declaring an integer variable with the value of 200
+amount_of_Goods = 200
+print(type(amount_of_Goods))
+
+#3.Declaring a String variable with the value of my name
+my_name = 'Latifat Yisa'
+print(type(my_name))
+
+
