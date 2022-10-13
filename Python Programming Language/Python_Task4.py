e783d4fe242f98ee683a02b1aaf38ee4174ca9a2
Latifat Yisa <latifat.yisa@accelerexholdings.com>
2022-10-14 00:05:37 +0100

    Python Task

---
+++ Python Programming Language/Python_Task4.py
@@ -0,0 +1,15 @@
+#Task 4
+
+#1. Declare two numbers with any values
+price1 = 2000
+price2 = 5000
+
+#2. Add or Subtract or Multiply the two numbers together
+sum = price1 + price2
+deficit = price1 - price2
+product = price1 * price2
+
+#3. Print out the value of the operation
+print(sum)
+print(deficit)
+print(product)
