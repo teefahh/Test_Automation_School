f776828a000006e90ba39ffcd8a4c58eca144312
Latifat Yisa <latifat.yisa@accelerexholdings.com>
2022-10-13 23:56:10 +0100

    Python Task

---
+++ Python Programming Language/Python_Task3.py
@@ -0,0 +1,19 @@
+#Task 3
+vacation_title1 = "An exquisite visit to the zoo\nwith the Zoology Team."
+
+# 1. Declaring a raw string
+vacation_title = R"An exquisite visit to the zoo\n with the Zoology Team."
+print(vacation_title)
+
+#2. Declaring two strings with any value
+book_title = "\nThe human body a temple as the house of Prayer"
+book_author = "\nby" + ' Latifat Yisa'
+#print(book_author)
+
+
+#3. Concatenating the two string
+selected_book = book_title + book_author
+
+
+#4. Print out the value of the concatenated strings
+print(selected_book)
