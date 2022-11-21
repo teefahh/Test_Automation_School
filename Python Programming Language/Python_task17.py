6b519940613ec48a9f8aa8515c9194534e9b29b1
teefahh <latifat.yisa@gmail.com>
2022-11-21 18:02:47 +0100

    Task update

---
+++ Python Programming Language/Python_task17.py
@@ -0,0 +1,23 @@
+print("OBJECT ORIENTED PROGRAMMING 1\n")
+
+
+class Human:
+
+    color = 'black'
+    group = 'mammals'
+    age = 20
+    age_group = 'mid 25\'s'
+    IQ = 'high'
+    gender = 'female'
+
+    # method
+    def human_attributes(self):
+        you = self.color()
+    print('Human\'s color is:', color)
+
+# object
+
+
+black = Human()
+print(black.color, black.group, black.age_group)
+
