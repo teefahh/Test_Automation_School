64b2f54c399177d02cf3b999bcc48d21a15af70f
teefahh <latifat.yisa@gmail.com>
2022-11-21 21:46:51 +0100

    Task update

---
+++ Python Programming Language/Python_task18.py
@@ -0,0 +1,15 @@
+print("OBJECT ORIENTED PROGRAMMING 2\n")
+
+
+class Human:
+
+    leg_count = 4
+    can_walk = True
+
+
+# class instantiation
+
+child = Human()
+print(child.leg_count)
+print(child.can_walk)
+
